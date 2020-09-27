b68e0271e747294c5840d7fa9d2b96569352f50e
Jiří Čech <jiricech94@gmail.com>
2020-09-27 12:56:22 +0200

    Add spec to layout generator

--- lib/generators/layout/layout_generator.rb
+++ lib/generators/layout/layout_generator.rb
@@ -2,35 +2,59 @@
 
 class LayoutGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
-  class_option :root_name, type: :string, default: 'homepage'
 
-  attr_reader :root_name
+  class_option :root_name, type: :string, default: 'homepage'
+  class_option :authenticated, type: :boolean, default: true
+
+  attr_reader :root_name, :authenticated
 
   def initialize_options
     @root_name = options['root_name']
+    @authenticated = options['authenticated']
   end
 
-  def template_files
-    template 'app/controllers/layout_controller.rb.tt',
-             "app/controllers/#{file_name}/#{file_name}_controller.rb"
+  def template_controllers
+    template(
+      'app/controllers/layout_controller.rb.tt',
+      "app/controllers/#{file_name}/#{file_name}_controller.rb"
+    )
 
-    template 'app/controllers/root_controller.rb.tt',
-             "app/controllers/#{file_name}/#{root_name}_controller.rb"
+    template(
+      'app/controllers/root_controller.rb.tt',
+      "app/controllers/#{file_name}/#{root_name}_controller.rb"
+    )
+  end
 
-    template 'app/views/layouts/layout.html.slim.tt',
-             "app/views/layouts/#{file_name}.html.slim"
+  def template_slim
+    template(
+      'app/views/layouts/layout.html.slim.tt',
+      "app/views/layouts/#{file_name}.html.slim"
+    )
 
-    template 'app/views/admin/root/index.html.slim.tt',
-             "app/views/#{file_name}/#{root_name}/index.html.slim"
+    template(
+      'app/views/admin/root/index.html.slim.tt',
+      "app/views/#{file_name}/#{root_name}/index.html.slim"
+    )
+  end
+
+  def template_spec
+    directory 'spec/support/shared_examples'
+
+    template(
+      'spec/requests/root_request_spec.rb.tt',
+      "spec/requests/#{file_name}/#{root_name}_request_spec.rb"
+    )
   end
 
   def inject_routes
     template 'config/routes.rb.tt', 'tmp/templates/config/routes.rb'
 
+    routes = File.read('tmp/templates/config/routes.rb') << "\n"
+
     inject_into_file(
       'config/routes.rb',
-      File.read('tmp/templates/config/routes.rb'),
-      after: "devise_for :users, skip: :omniauth_callbacks\n\n"
+      routes,
+      before: "get '/:locale'"
     )
 
     remove_file 'tmp/templates/config/routes.rb'
--- spec/requests/admin/dashboard_request_spec.rb
+++ spec/requests/admin/dashboard_request_spec.rb
@@ -3,5 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Dashboard', type: :request do
-  include_examples 'GET admin examples', '/admin'
+  include_examples 'GET authenticated examples',
+                   '/admin'
 end
