ab8a93263536430e88f871e70df702e6c8022218
Jiří Čech <jiricech94@gmail.com>
2020-10-30 19:08:37 +0100

    Refactor specs

--- spec/support/shared_examples/does_not_add_a_resource_example.rb
+++ spec/support/shared_examples/does_not_add_a_resource_example.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'does not create a new record' do |attributes|
-  let(:params) { { user: send(attributes) } }
-
   it 'does not create a new record' do
+    params = {}
+    params[resource_singular_symbol] = send(attributes)
+
     expect do
       post url_for(action: :create), params: params
     end.not_to change(model.all, :count)
